7b084074413b767ae8332c1940e698b90944e012
Santiago Bandiera <sagaban@hotmail.com>
2020-02-27 15:38:49 -0300

    Alter beeween blue and green squares

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,44 @@ use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
 use sdl2::rect::Rect;
-use sdl2::render::{Texture, TextureCreator};
+use sdl2::render::{Canvas, Texture, TextureCreator};
+use sdl2::video::{Window, WindowContext};
+
 use std::thread::sleep;
-use std::time::Duration;
+use std::time::{Duration, SystemTime};
 
 const TEXTURE_SIZE: u32 = 32;
+
+#[derive(Clone, Copy)]
+enum TextureColor {
+    Green,
+    Blue,
+}
+
+fn create_texture_rect<'a>(
+    canvas: &mut Canvas<Window>,
+    texture_creator: &'a TextureCreator<WindowContext>,
+    color: TextureColor,
+    size: u32,
+) -> Option<Texture<'a>> {
+    // We'll want to handle failures outside of this function.
+    if let Ok(mut square_texture) = texture_creator.create_texture_target(None, size, size) {
+        canvas
+            .with_texture_canvas(&mut square_texture, |texture| {
+                match color {
+                    // For now, TextureColor only handles two colors.
+                    TextureColor::Green => texture.set_draw_color(Color::RGB(0, 255, 0)),
+                    TextureColor::Blue => texture.set_draw_color(Color::RGB(0, 0, 255)),
+                }
+                texture.clear();
+            })
+            .expect("Failed to color a texture");
+        Some(square_texture)
+    } else {
+        // An error occurred so we return nothing and let the function caller handle the error.
+        None
+    }
+}
 
 pub fn main() {
     // initialize an SDL context
@@ -20,7 +53,7 @@ pub fn main() {
         .expect("Couldn't get SDL video subsystem");
 
     let window = video_subsystem
-        .window("rust-sdl2 demo: Video", 800, 600)
+        .window("Tetris", 800, 600)
         .position_centered()
         .opengl()
         .build()
@@ -35,16 +68,24 @@ pub fn main() {
 
     let texture_creator: TextureCreator<_> = canvas.texture_creator();
 
-    let mut square_texture: Texture = texture_creator
-        .create_texture_target(None, TEXTURE_SIZE, TEXTURE_SIZE)
-        .expect("Failed to create a texture");
+    // We create a texture with a 32x32 size.
+    let green_square = create_texture_rect(
+        &mut canvas,
+        &texture_creator,
+        TextureColor::Green,
+        TEXTURE_SIZE,
+    )
+    .expect("Failed to create a texture");
 
-    canvas
-        .with_texture_canvas(&mut square_texture, |texture| {
-            texture.set_draw_color(Color::RGB(0, 255, 0));
-            texture.clear();
-        })
-        .expect("Failed to color a texture");
+    let blue_square = create_texture_rect(
+        &mut canvas,
+        &texture_creator,
+        TextureColor::Blue,
+        TEXTURE_SIZE,
+    )
+    .expect("Failed to create a texture");
+
+    let timer = SystemTime::now();
 
     let mut event_pump = sdl_context
         .event_pump()
@@ -62,12 +103,30 @@ pub fn main() {
             }
         }
 
+        // We fill our window with red.
         canvas.set_draw_color(Color::RGB(255, 0, 0));
+        // We draw it.
         canvas.clear();
 
+        // The rectangle switch happens here:
+        let display_green = match timer.elapsed() {
+            Ok(elapsed) => elapsed.as_secs() % 2 == 0,
+            Err(_) => {
+                // In case of error, we do nothing...
+                true
+            }
+        };
+
+        let square_texture = if display_green {
+            &green_square
+        } else {
+            &blue_square
+        };
+
+        // Copy our texture into the window.
         canvas
             .copy(
-                &square_texture,
+                square_texture,
                 None,
                 // We copy it at the top-left of the window with a 32x32 size.
                 Rect::new(0, 0, TEXTURE_SIZE, TEXTURE_SIZE),
@@ -76,6 +135,8 @@ pub fn main() {
         // We update window's display.
         canvas.present();
 
+        // We sleep enough to get ~60 fps. If we don't call this, the program will take
+        // 100% of a CPU time.
         sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
 }
